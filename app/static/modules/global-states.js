fcd0a31e6dd86a0e0e7d06ff57eb15011f5c76ff
satilpereira <satil.prog@gmail.com>
2025-07-25 16:36:04 -0300

    add target state management and update UI; refactor package scripts for concurrent execution

--- app/static/modules/global-states.js
+++ app/static/modules/global-states.js
@@ -3,5 +3,7 @@ import { State, effect as globalEffect } from "./state.js";
 const speed = new State(3);
 const playback = new State("stop");
 const theme = new State("light");
+const target = new State(0);
+const algoType = new State("search"); // Default algorithm type e.g., "search", "sort", "graph"
 
-export { speed, playback, theme, globalEffect };
+export { speed, playback, theme, target, globalEffect };
